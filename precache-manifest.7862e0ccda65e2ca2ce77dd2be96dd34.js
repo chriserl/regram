2c7aa1457c5318576652656e470118efafc11214
chriserl <chriserldev@gmail.com>
2020-08-20 22:26:43 +0000

    Updates

---
+++ precache-manifest.7862e0ccda65e2ca2ce77dd2be96dd34.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edcc6c6fd058b93109f6b3b26b6b8089",
+    "url": "/regram/index.html"
+  },
+  {
+    "revision": "d6ea18d3c600854d7790",
+    "url": "/regram/static/css/main.582dc9d4.chunk.css"
+  },
+  {
+    "revision": "03ba5541eabc2b5a531b",
+    "url": "/regram/static/js/2.3a76d11f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/regram/static/js/2.3a76d11f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6ea18d3c600854d7790",
+    "url": "/regram/static/js/main.a1093e79.chunk.js"
+  },
+  {
+    "revision": "11e99c6c3dd0e030313e",
+    "url": "/regram/static/js/runtime-main.f568283e.js"
+  },
+  {
+    "revision": "52b04f1c2bd73f240b4ad620924a40c9",
+    "url": "/regram/static/media/Billabong.52b04f1c.ttf"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/regram/static/media/PoppinsLight.2a47a29c.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/regram/static/media/PoppinsMedium.ba95810b.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/regram/static/media/PoppinsRegular.41e8dead.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/regram/static/media/PoppinsSemiBold.342ba3d8.ttf"
+  },
+  {
+    "revision": "580b9bc9ce8f36172af36f7913cc53de",
+    "url": "/regram/static/media/bell.580b9bc9.svg"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/regram/static/media/cancel.cad70081.svg"
+  },
+  {
+    "revision": "e41f997a1661d3538a25b774622f5430",
+    "url": "/regram/static/media/envelope.e41f997a.svg"
+  },
+  {
+    "revision": "625282be4ef66e49dd4938651e357cb6",
+    "url": "/regram/static/media/hdimage.625282be.png"
+  },
+  {
+    "revision": "7746f5e4d82a2cf71a3d1f30fdef6ace",
+    "url": "/regram/static/media/heart.7746f5e4.svg"
+  },
+  {
+    "revision": "825269601201a0ece278db25a848c8d3",
+    "url": "/regram/static/media/instagram.82526960.svg"
+  },
+  {
+    "revision": "003fa517450f1e8894c908f0b272df70",
+    "url": "/regram/static/media/jade.003fa517.jpg"
+  },
+  {
+    "revision": "e75855a18e3532ccada0d25685656a95",
+    "url": "/regram/static/media/logout.e75855a1.svg"
+  },
+  {
+    "revision": "b2feb1c702b9b4c46343cfc222770c0d",
+    "url": "/regram/static/media/loupe.b2feb1c7.svg"
+  },
+  {
+    "revision": "4a9a687c86f334efc7fe8b6ce7dae6a4",
+    "url": "/regram/static/media/menu.4a9a687c.svg"
+  },
+  {
+    "revision": "ca394ec8a4754ab9db293dcac626bdcb",
+    "url": "/regram/static/media/menu.ca394ec8.svg"
+  },
+  {
+    "revision": "c65f6a86d450fc4507e91459539e36da",
+    "url": "/regram/static/media/play.c65f6a86.svg"
+  },
+  {
+    "revision": "c1cfd5169f240aadc17ea0ad75525ae6",
+    "url": "/regram/static/media/prof1.c1cfd516.jpg"
+  },
+  {
+    "revision": "500cbff9eeaa732bd74e08edb51e389f",
+    "url": "/regram/static/media/prof11.500cbff9.jpg"
+  },
+  {
+    "revision": "7acd4550c0a1da0c0acffc6aa4e6f809",
+    "url": "/regram/static/media/prof2.7acd4550.jpg"
+  },
+  {
+    "revision": "68fdbeaed3a19e2a111b69b712e64afe",
+    "url": "/regram/static/media/prof3.68fdbeae.jpg"
+  },
+  {
+    "revision": "09948ef9f030f2742a1dac7e93797d49",
+    "url": "/regram/static/media/prof4.09948ef9.jpg"
+  },
+  {
+    "revision": "0370e435a6ab61c3900215e42663be9d",
+    "url": "/regram/static/media/prof5.0370e435.jpg"
+  },
+  {
+    "revision": "7f705ae89ac3b97a268450ffe55c8943",
+    "url": "/regram/static/media/prof6.7f705ae8.jpg"
+  },
+  {
+    "revision": "d5c4675179b334df14d30fe3c9433312",
+    "url": "/regram/static/media/prof7.d5c46751.jpg"
+  },
+  {
+    "revision": "362b8d22bf50c9655bde3166ae289c0f",
+    "url": "/regram/static/media/prof8.362b8d22.jpg"
+  },
+  {
+    "revision": "c72d027f6677dc4e692f68a48619c84d",
+    "url": "/regram/static/media/prof9.c72d027f.jpg"
+  },
+  {
+    "revision": "b76d3403c67ff2f928ea5aaca2063e06",
+    "url": "/regram/static/media/saved.b76d3403.svg"
+  },
+  {
+    "revision": "0ec68f32e56d9b9837b8135c646f32e5",
+    "url": "/regram/static/media/speech-bubble.0ec68f32.svg"
+  }
+]);
